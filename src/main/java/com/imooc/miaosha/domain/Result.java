908956662cfd2f8b38f8f306ed0716317b950d6d
kiudou <qidong120@hotmail.com>
2018-11-12 21:53:34 +0800

    增加了对密码MD5的方法

---
+++ src/main/java/com/imooc/miaosha/domain/Result.java
@@ -0,0 +1,64 @@
+package com.imooc.miaosha.domain;
+
+import java.io.Serializable;
+
+public class Result<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * 数据
+     */
+    private T data;
+    /**
+     * 信息
+     */
+    private String message;
+    /**
+     * 是否成功
+     */
+    private boolean success;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Result() {
+        super();
+    }
+
+    public Result(T data, String message, boolean success) {
+        this.data = data;
+        this.message = message;
+        this.success = success;
+    }
+
+    public Result(T data, String message) {
+        this.data = data;
+        this.message = message;
+        this.success = true;
+    }
+
+    public Result(T data) {
+        this.data = data;
+        this.success = true;
+    }
+}
---
+++ src/main/java/com/imooc/miaosha/util/MD5Util.java
@@ -0,0 +1,32 @@
+package com.imooc.miaosha.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class MD5Util {
+
+    public static String md5(String str) {
+        return DigestUtils.md5Hex(str);
+    }
+
+    public static final String salt = "1a2b3c4d";
+
+    public static String inputPassToFormPass(String inputPass){
+        String str = salt.charAt(0) + salt.charAt(2) + inputPass
+                + salt.charAt(5) + salt.charAt(4);
+        return md5(str);
+    }
+
+    public static String formPassToDBPass(String formPass, String DBsalt) {
+        String str = DBsalt.charAt(0) + DBsalt.charAt(2) + formPass
+                + DBsalt.charAt(5) + DBsalt.charAt(4);
+        return md5(str);
+    }
+
+    public static String inputPassToDBPass(String inputPass, String DBSalt) {
+        return formPassToDBPass(inputPassToFormPass(inputPass), DBSalt);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(inputPassToDBPass("123456","1a2b3c4d"));
+    }
+}
