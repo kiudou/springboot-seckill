7df96746c9d44f5c7e5465d64385829ee6165c83
kiudou <qidong120@hotmail.com>
2018-11-28 19:35:30 +0800

    增加了war包的打包依赖和启动类的更新

--- src/main/java/com/imooc/miaosha/MiaoshaApplication.java
+++ src/main/java/com/imooc/miaosha/MiaoshaApplication.java
@@ -2,11 +2,18 @@ package com.imooc.miaosha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MiaoshaApplication {
+public class MiaoshaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiaoshaApplication.class, args);
+	}
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder builder) { //打成war包得加这个
+		return builder.sources(MiaoshaApplication.class);
 	}
 }
