a54cb1db9cd943561480d83f36af142d1f505d05
kiudou <qidong120@hotmail.com>
2018-11-29 14:02:28 +0800

    在goods_list页面增加了页面缓存

--- src/main/java/com/imooc/miaosha/controller/GoodsController.java
+++ src/main/java/com/imooc/miaosha/controller/GoodsController.java
@@ -4,6 +4,8 @@ import com.imooc.miaosha.domain.CodeMsg;
 import com.imooc.miaosha.domain.MiaoshaUser;
 import com.imooc.miaosha.domain.Result;
 import com.imooc.miaosha.domain.User;
+import com.imooc.miaosha.redis.GoodsKey;
+import com.imooc.miaosha.redis.RedisService;
 import com.imooc.miaosha.service.GoodsService;
 import com.imooc.miaosha.service.MiaoshaUserService;
 import com.imooc.miaosha.vo.GoodsVo;
@@ -11,11 +13,17 @@ import com.imooc.miaosha.vo.LoginVo;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.context.webflux.SpringWebFluxContext;
+import org.thymeleaf.spring5.context.webmvc.SpringWebMvcThymeleafRequestContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
@@ -32,15 +40,39 @@ public class GoodsController {
     @Resource
     GoodsService goodsService;
 
+    @Resource
+    RedisService redisService;
 
-    @RequestMapping("/to_list")
-    public String toList(Model model, MiaoshaUser user) {
+    @Resource
+    ThymeleafViewResolver thymeleafViewResolver;
+
+    @Resource
+    ApplicationContext applicationContext;
+
+    @RequestMapping(value = "/to_list", produces = "text/html")
+    @ResponseBody
+    public String toList(HttpServletRequest request, HttpServletResponse response,Model model, MiaoshaUser user) {
         model.addAttribute("user", user);
 
         //查询秒杀列表
         List<GoodsVo> goodsList =  goodsService.listGoodsVo();
         model.addAttribute("goodsList", goodsList);
-        return "goods_list";
+//        return "goods_list";
+
+
+        //页面缓存，用于短时间并发量高的情况
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsList,"",String.class);
+        if(!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        //手动渲染
+        WebContext context = new WebContext(request, response, request.getServletContext(),request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", context);
+        if(!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsList, "", html);
+        }
+        return html;
     }
 
     @RequestMapping("/to_detail/{goodsId}")
---
+++ src/main/java/com/imooc/miaosha/redis/GoodsKey.java
@@ -0,0 +1,15 @@
+package com.imooc.miaosha.redis;
+
+public class GoodsKey extends BasePrefix {
+
+    public GoodsKey() {
+    }
+
+    public GoodsKey(int expireSeconds, String prefix) {
+        super(expireSeconds,prefix);
+    }
+
+    public static GoodsKey getGoodsList = new GoodsKey(60,"gl");
+
+
+}
